a61b4b8d607b19b6ca5ab856050aff3534c3516d
Casper Burik <cburik@users.noreply.github.com>
2025-12-14 16:39:58 +0100

    template

---
+++ src/advent_tools/cli.py
@@ -0,0 +1,46 @@
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from subprocess import Popen
+
+BASE_DIR = Path(__file__).resolve().parents[1] / "solutions"
+TEMPLATE_PATH = Path(__file__).parent / "templates" / "solution_template.py"
+
+def parse_args() -> Namespace:
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("-y", type=int, help="year", required=True)
+    arg_parser.add_argument("-d", type=int, help="day", required=True)
+    args = arg_parser.parse_args()
+    if args.y < 100:
+        args.y += 2000
+    return args
+
+def create_files(year:int, day:int):
+    basepath = BASE_DIR / str(year) / f"{day:02d}"
+    solution_file = basepath / "solution.py"
+    test_data_file = basepath / "data" / "test_data.txt"
+    data_file = basepath / "data" / "data.txt"
+
+    data_file.parent.mkdir(parents=True, exist_ok=True)
+    if not solution_file.exists():
+        with open(solution_file, "w") as f:
+            f.write("# Write your solution here\n")
+    if not test_data_file.exists():
+        with open(test_data_file, "w") as f:
+            f.write("")
+    if not data_file.exists():
+        with open(data_file, "w") as f:
+            f.write("")
+
+def open_files(year:int, day:int):
+    basepath = BASE_DIR / str(year) / f"{day:02d}"
+    solution_file = basepath / "solution.py"
+    test_data_file = basepath / "data" / "test_data.txt"
+    data_file = basepath / "data" / "data.txt"
+
+    Popen(["code", str(solution_file), str(test_data_file), str(data_file)])
+
+def main():
+    args = parse_args()
+    create_files(args.y, args.d)
+    open_files(args.y, args.d)
+
---
+++ src/templates/solution_template.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+def solve_part1(data: list[str]) -> int:
+    pass
+
+def solve_part2(data: list[str]) -> int:
+    pass
+
+def get_input_data(test = False) -> list[str]:
+    data_file = Path(__file__).parent / "data" / ("test_data.txt" if test else "data.txt")
+    with open(data_file) as f:
+        data = f.readlines()
+    return data
+
+if __name__ == "__main__":
+    data = get_input_data(test=False)
+    result_part1 = solve_part1(data)
+    print(f"Part 1: {result_part1}")
+    result_part2 = solve_part2(data)
+    print(f"Part 2: {result_part2}")
